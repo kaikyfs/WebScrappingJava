dfd3c80f53dddebf40fe7feaed14ce917bf4edcc
Kaiky Ferreira <146747941+kaikyfs@users.noreply.github.com>
2023-10-29 23:56:20 -0300

    Update App.java

--- mavenBasico/src/main/java/br/upe/App.java
+++ mavenBasico/src/main/java/br/upe/App.java
@@ -1,10 +1,17 @@
 package br.upe;
 
 public class App {
-  public static void main(String[] args) {
-    String[] resultado = ResultadoMegaSena.obtemUltimoResultado();
-    for (String dezena : resultado) {
-      System.out.print(dezena + " ");
+    public static void main(String[] args) {
+        String[] dezenas = null;
+
+        while (dezenas == null) {
+            dezenas = ResultadoMegaSena.obtemUltimoResultado();
+        }
+        System.out.println("Dezenas da Mega Sena:");
+        for (String dezena : dezenas) {
+            System.out.print(dezena + " ");
+            System.out.println("\n");
+
+        }
     }
-  }
 }
